f10ff9bdf07ef4a34d8261c5735d730c93a3b2ff
aleze89 <68794504+aleze89@users.noreply.github.com>
2024-05-20 23:46:05 -0300

    validación y mensajes funcionan correctamente

--- js/index.js
+++ js/index.js
@@ -20,49 +20,49 @@ formRegister.addEventListener("submit",e=>{
     let regexPhone = /^\d+$/;
 
 
-if(inputNombre.value.length<3){
-   document.getElementById("error-firstname").innerHTML = `El nombre es corto`;
-   valor=true;
-}
-
-if(inputApellido.value.length<3){
-    warning+=`El apellido es corto<br>`
-    valor=true;
-}
-
-if(!regexEmail.test(inputEmail.value)){
-    warning+=`El email no es valido<br>`
-    valor=true;
-}
-
-if (!regexPhone.test(inputTelefono.value) || inputTelefono.value.length < 10) {
-    if (!regexPhone.test(inputTelefono.value)) {
-        warning += `El teléfono solo debe contener números<br>`;
-    } else {
-        warning += `El teléfono no es valido<br>`;
+    if(inputNombre.value.length<3){
+        document.getElementById("error-firstname").innerHTML = `El nombre es corto`;
+        valor = true;
     }
-    valor = true;
-}
 
-if(inputComunicacion.value=="default"){
-    warning+=`Elija una opcion <br>`
-    valor=true;
-}
+    if(inputApellido.value.length<3){
+        document.getElementById("error-lastname").innerHTML = `El apellido es corto`;
+        valor = true;
+    }
 
-if(inputCurso.value=="default"){
-    warning+=`Elija una opcion <br>`
-    valor=true;
-}
+    if(!regexEmail.test(inputEmail.value)){
+        document.getElementById("error-email").innerHTML = `El email no es valido`;
+        valor = true;
+    }
 
-if(inputMensaje.value.length<10) {
-    warning+=`Que mensaje quiere dejarnos?<br>`
-    valor=true;
-}
+    if (!regexPhone.test(inputTelefono.value) || inputTelefono.value.length < 10) {
+        if (!regexPhone.test(inputTelefono.value)) {
+            document.getElementById("error-phone").innerHTML = `El teléfono solo debe contener números`;
+        } else {
+            document.getElementById("error-phone").innerHTML = `El teléfono no es valido`;
+        }
+        valor = true;
+    }
 
-if(valor){
-    parrafo.innerHTML=warning;
-}else{
-    parrafo.innerHTML="Enviado";
-    formRegister.reset();
-}
-})
+    if(!document.querySelector('input[name="contact-choice"]:checked')) {
+        document.getElementById("error-comunicacion").innerHTML = "Elija una opción de comunicación";
+        valor = true;
+    }
+
+    if(inputCurso.value == ""){
+        document.getElementById("error-product").innerHTML = "Elija una opción";
+        valor = true;
+    }
+
+    if(inputMensaje.value.length < 10) {
+        document.getElementById("error-message").innerHTML = "¿Qué mensaje quiere dejarnos?";
+        valor = true;
+    }
+
+    if ( valor ) {
+        statusMessage.innerHTML= "";
+    } else {
+        statusMessage.innerHTML="Enviado";
+        formRegister.reset();
+    }
+});
