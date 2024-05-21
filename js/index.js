b244035b76baff73a1ffaede96f1409cfd48d6f1
aleze89 <68794504+aleze89@users.noreply.github.com>
2024-05-21 18:01:50 -0300

    Bloqueo de envio a menos q se acepten los "términos"

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const inputTelefono=document.getElementById("phone");
 const inputComunicacion=document.getElementById("comunicacion");
 const inputCurso=document.getElementById("product");
 const inputMensaje=document.getElementById("message");
+const inputTerms=document.getElementById("terms");
 const statusMessage=document.getElementById("status-message");
 
 
@@ -19,22 +20,25 @@ formRegister.addEventListener("submit",e=>{
     let regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let regexPhone = /^\d+$/;
 
-
+    //Nombre
     if(inputNombre.value.length<3){
         document.getElementById("error-firstname").innerHTML = `El nombre es corto`;
         valor = true;
     }
 
+    //Apellido
     if(inputApellido.value.length<3){
         document.getElementById("error-lastname").innerHTML = `El apellido es corto`;
         valor = true;
     }
 
+    //Email
     if(!regexEmail.test(inputEmail.value)){
         document.getElementById("error-email").innerHTML = `El email no es valido`;
         valor = true;
     }
 
+    //Telefono
     if (!regexPhone.test(inputTelefono.value) || inputTelefono.value.length < 10) {
         if (!regexPhone.test(inputTelefono.value)) {
             document.getElementById("error-phone").innerHTML = `El teléfono solo debe contener números`;
@@ -44,21 +48,31 @@ formRegister.addEventListener("submit",e=>{
         valor = true;
     }
 
+    //Medio de comunicación
     if(!document.querySelector('input[name="contact-choice"]:checked')) {
         document.getElementById("error-comunicacion").innerHTML = "Elija una opción de comunicación";
         valor = true;
     }
 
+    //Curso
     if(inputCurso.value == ""){
         document.getElementById("error-product").innerHTML = "Elija una opción";
         valor = true;
     }
 
+    //Mensaje
     if(inputMensaje.value.length < 10) {
         document.getElementById("error-message").innerHTML = "¿Qué mensaje quiere dejarnos?";
         valor = true;
     }
 
+    //Terminos
+    if(!inputTerms.checked) {
+        statusMessage.innerHTML = "Debe aceptar la política de privacidad";
+        valor = true;
+    }
+
+    //Error o Envio
     if ( valor ) {
         statusMessage.innerHTML= "";
     } else {
