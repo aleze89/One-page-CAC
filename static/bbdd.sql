6e4e3a0fdce3f744f0d6f987a28dc828768ef0e5
TadeoRiganti <tadeoriganti@gmail.com>
2024-06-30 02:46:18 -0300

    FEAT Agregar atributos en la tabla 'estudiantes'

--- static/bbdd.sql
+++ static/bbdd.sql
@@ -2,7 +2,16 @@ CREATE DATABASE aprendev;
 
 USE aprendev;
 
-CREATE TABLE estudiantes ();
+CREATE TABLE estudiantes (
+    dni int(11),
+    nombre varchar(30),
+    apellido varchar(30),
+    fecha_nacimiento date,
+    correo_electronico varchar(45),
+    telefono varchar(20),
+    pais varchar(50),
+    foto_perfil varchar(255)
+);
 
 CREATE TABLE cursos ();
 
