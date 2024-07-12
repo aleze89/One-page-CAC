cb6edea7939ed123711f5651c74eb4e180bd61c4
TadeoRiganti <tadeoriganti@gmail.com>
2024-07-12 01:54:41 -0300

    STYLE: Agregar una funcion clamp al ancho con el cual se carga la imagen

--- static/js/crud-testimonios-fetch.js
+++ static/js/crud-testimonios-fetch.js
@@ -96,7 +96,7 @@ async function showTestimonios(){
                   <td>${testimonio.comentario}</td>
                   <td>${testimonio.fecha_publicacion}</td>
                   <td>
-                      <img src="${testimonio.foto_perfil}" width="30%">
+                      <img src="${testimonio.foto_perfil}" width: clamp(32px, 64px, 30%)>
                   </td>
                   <td>
                       <button class="btn-cac" onclick='updateTestimonio(${testimonio.id_testimonio})'><i class="fa fa-pencil" ></button></i>
